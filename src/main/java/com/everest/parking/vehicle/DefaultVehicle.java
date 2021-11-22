1cd339cbf34b9d6e4fe599b2576966d007865b46
rajesh <raaj.everest@gmail.com>
2021-11-22 18:29:29 +0530

    added DefaultVehicle interface

---
+++ src/main/java/com/everest/parking/vehicle/DefaultVehicle.java
@@ -0,0 +1,11 @@
+package com.everest.parking.vehicle;
+
+public interface DefaultVehicle {
+
+    String getRegistrationNumber();
+
+    String getTypeOfVehicle();
+
+    String getColourOfVehicle();
+
+}
