97b09a02ad19994fad6f61067a6b0f69cc2efab2
rajesh <raaj.everest@gmail.com>
2021-11-22 16:18:23 +0530

    intial commit

---
+++ src/main/java/com/everest/parking/App.java
@@ -0,0 +1,7 @@
+package com.everest.parking;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/everest/parking/AppTest.java
@@ -0,0 +1,7 @@
+package com.everest.parking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+}
