c34bd21013287692b094a988f37f2b699551e084
rajesh <raaj.everest@gmail.com>
2021-11-23 12:14:47 +0530

    added slot object with testing

---
+++ src/main/java/com/everest/parking/parkinglot/Slot.java
@@ -0,0 +1,27 @@
+package com.everest.parking.parkinglot;
+
+import com.everest.parking.vehicle.Vehicle;
+
+public class Slot {
+
+    private boolean isOccupied;
+    private Vehicle vehicleInSlot;
+
+    public Slot(Vehicle vehicleInSlot) {
+        this.vehicleInSlot = vehicleInSlot;
+        this.isOccupied = true;
+    }
+
+    public boolean getIsOccupied() {
+        return isOccupied;
+    }
+
+    public Vehicle getVehicleInSlot() {
+        return vehicleInSlot;
+    }
+
+    public void removeVehicleInSlot() {
+        this.vehicleInSlot = null;
+        this.isOccupied = false;
+    }
+}
---
+++ src/test/java/com/everest/parking/parkinglot/SlotTest.java
@@ -0,0 +1,33 @@
+package com.everest.parking.parkinglot;
+
+import com.everest.parking.vehicle.types.Car;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SlotTest {
+
+    Slot slot;
+
+    @BeforeEach
+    void setSlot(){
+        slot = new Slot(new Car("WA-KA-WA-KA","RED"));
+    }
+
+    @Test
+    void getIsOccupiedTest(){
+        assertTrue(slot.getIsOccupied(),"Slot isOccupied must be true");
+    }
+
+    @Test
+    void removeVehicleInSlotTest(){
+        slot.removeVehicleInSlot();
+        assertFalse(slot.getIsOccupied(),"the slot must be free");
+        assertNull(slot.getVehicleInSlot());
+    }
+
+
+
+
+}
