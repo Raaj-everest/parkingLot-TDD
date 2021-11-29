fdbbd87349ffd6a9b7d22b2c87ac611653aedfeb
rajesh <raaj.everest@gmail.com>
2021-11-29 13:13:48 +0530

    added create_parking_lot command

--- src/main/java/com/everest/parking/App.java
+++ src/main/java/com/everest/parking/App.java
@@ -3,6 +3,7 @@ package com.everest.parking;
 import com.everest.parking.InputHandler.InputHandler;
 import com.everest.parking.parkinglot.ParkingLot;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,11 +15,17 @@ public class App {
         do {
             Scanner sc = new Scanner(System.in);
             String str = sc.nextLine();
-            List<String> newStr = InputHandler.input(str);
-            if(newStr.contains("EXIT")){
-                exit=true;
+            ArrayList<String> newStr = (ArrayList<String>) InputHandler.input(str);
+            if(newStr.contains("EXIT")) {
+                exit = true;
                 System.out.println("you terminated the programme");
                 sc.close();
+            }else if(system==null){
+                if (newStr.contains("CREATE_PARKING_LOT")) {
+                    system = InputHandler.buildParkingLot(newStr);
+                } else {
+                    System.out.println("Create a ParkingLot to continue");
+                }
             }else{
 
             }
--- src/main/java/com/everest/parking/InputHandler/InputHandler.java
+++ src/main/java/com/everest/parking/InputHandler/InputHandler.java
@@ -1,20 +1,38 @@
 package com.everest.parking.InputHandler;
 
+import com.everest.parking.parkinglot.ParkingLot;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class InputHandler {
-    public static List<String> input(String str){
+    public static ArrayList<String> input(String str){
         str = str.toUpperCase();
         str = str.trim();
         String[] newStr = str.split("\\s+");
-        ArrayList<String> list = (ArrayList<String>) Arrays.asList(newStr);
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list,newStr);
         return list;
     }
+    public static ParkingLot buildParkingLot(List<String> str){
+        if(str.contains("CREATE_PARKING_LOT")) {
+            try {
+                String id = str.get(1);
+                int numberOfFloors = Integer.parseInt(str.get(2));
+                int numberOfSlotsPerFloor = Integer.parseInt(str.get(3));
+                return new ParkingLot(id, numberOfFloors, numberOfSlotsPerFloor);
+            }catch (Exception e){
+                System.out.println("please give id,number of floors and number of slots per floor correctly");
+            }
+        }
+        return null;
+    }
     public static void doWork(List<String> str){
-        if(str.contains("CREATE_PARKING_LOT")){
+        if(str.contains("DISPLAY")){
 
         }
     }
+
 }
