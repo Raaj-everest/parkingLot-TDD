7d3c61473c65ab8194e5eedc15fc51d54d44987c
rajesh <raaj.everest@gmail.com>
2021-11-30 17:11:25 +0530

    guard clause added to ticket

--- src/main/java/com/everest/parking/parkinglot/services/Ticket.java
+++ src/main/java/com/everest/parking/parkinglot/services/Ticket.java
@@ -5,11 +5,12 @@ public class Ticket {
     public void generateTicket(int[] place, String parkingLotId, int numberOfSlotsPerFloor, int numberOfFloors) {
         if(place==null){
             System.out.println("Parking Lot Full");
-        }else {
+            return;
+        }
             int floorNumber = place[0];
             int slotNumber = place[1];
             String ticket = (parkingLotId + "_" + (floorNumber + 1) + "_" + (slotNumber + 1));
             System.out.println("Parked vehicle. Ticket ID: " + ticket);
-        }
+
     }
 }
